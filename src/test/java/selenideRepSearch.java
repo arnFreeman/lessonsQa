a43b088b343bccbeec3ecffb2b552c895873b2c8
RenatAbdullin <arn_freeman@mail.ru>
2024-02-16 00:40:11 +0500

    don't work

--- src/test/java/selenideRepSearch.java
+++ src/test/java/selenideRepSearch.java
@@ -2,14 +2,15 @@ import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selenide.*;
 
-public class SelenideRepositorySearch {
+public class selenideRepSearch {
     @Test
     void shouldFindSelenideRepositoryAtTheTop() {
 
         open("https://github.com/");
         $("[placeholder='Search or jump to...']").click();
         $("[name='query-builder-test']").setValue("selenide").pressEnter();
-        $$("results-list").first().$("/a").click();
+        $$("[class='Box-sc-g0xbh4-0 kXssRI']").first().$("Text-sc-17v1xeu-0 qaOIC search-match").click();
+
         sleep(5000);
 
 
