0da31f4e5d8dfdfe74fa8468ca2dc0cb7d5c682e
RenatAbdullin <arn_freeman@mail.ru>
2024-02-20 22:37:55 +0500

    fix TakeEnterpriseByHover and DragAndDropTest

--- src/test/java/GithubJunitcodeSearchTest.java
+++ src/test/java/GithubJunitcodeSearchTest.java
@@ -3,7 +3,6 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
 
 
@@ -32,6 +31,6 @@ public class GithubJunitcodeSearchTest {
                     $("#first").should(visible).click();
                     $("#second").should(visible).click();
                   }
-                }""")).shouldBe(visible);
+                }"""));
     }
 }
